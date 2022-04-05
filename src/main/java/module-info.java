aefba286fb74b23166e5c7df6bba3397d3f68f4e
firecharged174 <zan2b@hotmail.com>
2022-04-04 20:53:24 -0400

    sdnfkdn

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.aoop_final_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.aoop_final_project to javafx.fxml;
+    exports com.example.aoop_final_project;
+}
