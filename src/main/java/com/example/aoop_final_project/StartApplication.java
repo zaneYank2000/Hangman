ce1ed51e9cbacc582b70a5b1ea15c8f3b72e7e82
Montage8789 <guitarcory@gmail.com>
2022-04-05 14:56:43 -0400

    added controller to switch from page to page

--- src/main/java/com/example/aoop_final_project/StartApplication.java
+++ src/main/java/com/example/aoop_final_project/StartApplication.java
@@ -2,17 +2,18 @@ package com.example.aoop_final_project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Welcome to Hangman!");
         stage.setScene(scene);
         stage.show();
     }
