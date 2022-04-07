c52325c054d171f68116bb36e78e3a9dd1eed3ae
Montage8789 <guitarcory@gmail.com>
2022-04-06 23:12:25 -0400

    added file to pass word

--- src/main/java/com/example/aoop_final_project/PopupController.java
+++ src/main/java/com/example/aoop_final_project/PopupController.java
@@ -9,12 +9,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class PopupController {
-    public ArrayList wordArray = new ArrayList<>();
 
     @FXML
     private Text errorText;
@@ -29,6 +33,21 @@ public class PopupController {
 
     @FXML
     void confirmButtonClicked(ActionEvent event) throws IOException {
+        try {
+            File wordFile = new File("src/main/resources/com/example/aoop_final_project", "word.txt");
+            FileWriter fw = new FileWriter(wordFile);
+            PrintWriter pw = new PrintWriter(fw);
+            String word = wordTextField.getText();
+            pw.print(word);
+            pw.close();
+        } catch (IOException e){
+            System.out.println("Error occurred.");
+            e.printStackTrace();
+        }
+
+
+
+        //TODO: FIX THIS IF STATEMENT
         if(1==1) {
             Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
             Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
