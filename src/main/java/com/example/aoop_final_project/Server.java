cfb5fa754f07a86247b167eb5435ec9785602fe4
Montage8789 <guitarcory@gmail.com>
2022-04-09 16:00:04 -0400

    first commit on server

---
+++ src/main/java/com/example/aoop_final_project/Server.java
@@ -0,0 +1,21 @@
+package com.example.aoop_final_project;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+    private static int maxClients = 2;
+
+    public static void main(String [] args) {
+        try {
+            ServerSocket serverSocket;
+
+            //get input for port
+
+            //create new serversocket and a pool of threads
+        } catch {
+
+        }
+
+    }
+}
