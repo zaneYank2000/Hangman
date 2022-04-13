23fbb710d79d3ceb0faaee69cb609efcdf76568a
Montage8789 <guitarcory@gmail.com>
2022-04-13 13:05:48 -0400

    tried login stuff

--- src/main/java/com/example/aoop_final_project/LoginController.java
+++ src/main/java/com/example/aoop_final_project/LoginController.java
@@ -11,7 +11,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class LoginController {
 
@@ -31,6 +37,14 @@ public class LoginController {
     private TextField usernameTextField;
 
     public void Login(ActionEvent event) throws IOException {
+
+        boolean found = false;
+        String tempusername = "";
+        String tempassword = "";
+        Scanner scan = new Scanner("members.txt");
+        scan.useDelimiter(",");
+
+
         if(usernameTextField.getText().equals("user") && passwordPasswordField.getText().equals("pass")) {
 
             //allow access
@@ -47,5 +61,22 @@ public class LoginController {
             errorText.setText("Username or password is incorrect");
 
         }
+    }
+
+    public void signUpButtonClicked(ActionEvent actionEvent) throws IOException {
+        String newusername = usernameTextField.getText();
+        String newpassword = passwordPasswordField.getText();
+        File wordFile = new File("members.txt");
+        FileWriter fw = new FileWriter(wordFile);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.write(newusername + "," + newpassword + "\n");
+        pw.write("\n");
+        pw.close();
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
     }
 }
